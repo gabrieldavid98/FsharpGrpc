ac927c0cf260031fd2ac5a3f9c9fbd39022778bd
Gabriel Mendoza <gabrieldavid_98@hotmail.com>
2023-11-16 23:32:12 -0500

    initial commit

---
+++ FsharpGrpc.Defs/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace FsharpGrpc.Defs
+
+open FsharpGrpc.Lib
+
+type GreetService() =
+    inherit Greeter.GreeterBase()
+
+    override _.SayHello(req, ctx) =
+        task {
+            let r = new HelloReply()
+            r.Message <- sprintf "Hello %s From F#" req.Name
+
+            return r
+        }
